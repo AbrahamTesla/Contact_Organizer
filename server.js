82a3dccc502d7ebf17cfd109fdb5c497dfdb07a5
Manlangit <manlangitv@gmail.com>
2022-01-20 14:48:49 -0500

    for redeployment

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
-// const wss = new SocketServer({ server });
+
 //Connect Database
 connectDB();
 
@@ -18,16 +18,16 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-//Server static assets in production. If it's in production load a static folder
-// if (process.env.NODE_ENV === 'productin') {
-//set static folder
-//    app.use(express.static('client/build'));
+// Server static assets in production. If it's in production load a static folder
+if (process.env.NODE_ENV === 'production') {
+   // First set a static folder
+   app.use(express.static('client/build'));
 
-// Route to '*' means get anything other than the above routes (e.g./api/user, /api/contacts/) look for 'index.html' under the client & build folder
-//    app.get('*', (req, res) =>
-//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//    );
-// }
+   // Route to '*' means get anything other than the above routes (e.g./api/user, /api/contacts/) look for 'index.html' under the client & build folder
+   app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+   );
+}
 
 const PORT = process.env.PORT || 5000;
 
